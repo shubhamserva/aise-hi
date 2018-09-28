248d8f7586e88bbdcd068e47f5c89d685c39d453
shubhamserva <34180935+shubhamserva@users.noreply.github.com>
2018-09-28 12:05:57 +0530

    Add files via upload

---
+++ Strings/Qwer.java
@@ -0,0 +1,21 @@
+package Strings;
+
+public class Qwer 
+{
+	public static void main(String args[])
+	{
+		String s = "mindtree";
+		s = s.concat("kalinga");
+		System.out.println(s);
+		
+		
+		
+		//		"	//	char ch[] = {'k','a','l','i','n','g','a'};
+	/*	String s2 = new String (ch);
+		String s3 = new String("mindtree");
+ 		System.out.println(s);
+		System.out.println(s2);
+		System.out.println(s3);*/
+	}
+
+}
